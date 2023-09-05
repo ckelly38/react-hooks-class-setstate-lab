35913408cf28d6171e443dd651e420e6b0326182
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-09-05 00:04:25 -0600

    used state and defined it on this, but event handlers must have this bound to it in someway

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 
-function Item({ name, category }) {
-  const [isInCart, setIsInCart] = useState(false);
-
-  function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+class Item extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.props = props;
+    this.state = {
+      isInCart: false,
+    };
   }
 
-  return (
-    <li className={isInCart ? "in-cart" : ""}>
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button
-        className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
-      >
-        {isInCart ? "Remove From" : "Add to"} Cart
-      </button>
-    </li>
-  );
+  handleAddToCartClick(event) {
+    this.setState({
+      isInCart: !this.state.isInCart
+    });
+  }
+
+  render()
+  {
+    return (
+      <li className={this.state.isInCart ? "in-cart" : ""}>
+        <span>{this.props.name}</span>
+        <span className="category">{this.props.category}</span>
+        <button
+          className={this.state.isInCart ? "remove" : "add"}
+          onClick={this.handleAddToCartClick.bind(this)}
+        >
+          {this.state.isInCart ? "Remove From" : "Add to"} Cart
+        </button>
+      </li>
+    );
+  }
 }
 
 export default Item;
